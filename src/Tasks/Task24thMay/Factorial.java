93e9142bc4db4998215c452b41e29092b9eec8cd
Payal Gaikwad <poyal1994@gmail.com>
2025-07-04 00:10:46 +0530

    24th May Task

--- src/Tasks/Task24thMay/Factorial.java
+++ src/Tasks/Task24thMay/Factorial.java
@@ -7,11 +7,18 @@ public class Factorial {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a number: ");
-        int n = scanner.nextInt();
-        int factorial = 1;
-        for (int i = 1; i <= n; i++) {
-            factorial *= i;
+        if(scanner.hasNextInt())
+        {
+            int n = scanner.nextInt();
+            int factorial = 1;
+            for (int i = 1; i <= n; i++) {
+                factorial *= i;
+            }
+            System.out.println("Factorial of " + n + " is: " + factorial);
         }
-        System.out.println("Factorial of " + n + " is: " + factorial);
+        else
+        {
+            System.out.println("Invalid input");
+        }
     }
 }
